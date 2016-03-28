fc7d6e6d0e049935311e3c78151b262d46baecd0
Ivan Shevelev <vanujkee@icloud.com>
2016-03-28 18:49:00 +0600

    add comments

--- Pod/Classes/HRAssert.h
+++ Pod/Classes/HRAssert.h
@@ -9,10 +9,31 @@
 #ifndef HRAssert_h
 #define HRAssert_h
 
+/**
+ *  Assert for check current class is kind of waiting class.
+ *
+ *  @param waiting Waiting value can be instance, Class type instance or class name(like NSString).
+ *  @param current Current value can be instance, Class type instance or class name(like NSString).
+ *
+ */
 #define HRKindOfClassAssert(waiting, current) NSAssert([waiting class] == [current class] || [[current class] isSubclassOfClass:[waiting class]], @"'%@' is not kind of '%@' class", @#current, @#waiting)
 
+/**
+ *  Assert for check instance conforms to protocol.
+ *
+ *  @param waiting Waiting value must be protocol name(like NSCopying).
+ *  @param current Current value can be instance, Class type instance or class name(like NSString).
+ *
+ */
 #define HRConformsToProtocolAssert(waiting, current) NSAssert([[current class] conformsToProtocol:@protocol(waiting)], @"'%@' do not support '%@' protocol", @#current, @#waiting);
 
+/**
+ *  Assert for check instance conforms to protocol.
+ *
+ *  @param waiting Waiting value must be Protocol type instance.
+ *  @param current Current value can be instance, Class type instance or class name(like NSString).
+ *
+ */
 #define HRConformsToProtocolTypeAssert(waiting, current) NSAssert([[current class] conformsToProtocol:waiting], @"'%@' do not support '%@' protocol", @#current, @#waiting);
 
 
